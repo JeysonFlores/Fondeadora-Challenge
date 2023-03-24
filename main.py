42b0e3ff514e961ed429a455755ac2e6d64e90c8
JeysonFlores <jeyson.flores@hotmail.com>
2023-03-23 21:45:24 -0700

    File formatted and cleaned

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 def destructure(array: list) -> list:
     for index, el in enumerate(array):
         if isinstance(el, list):
-            array[index:(index+1)] = destructure(el)
+            array[index : (index + 1)] = destructure(el)
     return array
-
-
-data = [1, [2, [3, [4, 5]]]]
-
-print(destructure(data))
